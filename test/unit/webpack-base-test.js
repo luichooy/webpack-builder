8b3f5f0c97399e30050902a79f8ea643650a77ad
luichooy <luichooy@gmail.com>
2020-03-29 01:12:57 +0800

    test: assert error

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -4,7 +4,7 @@ describe('webpack-base.js test case', () => {
     const baseConfig = require('../../lib/webpack.base')
     
     it('entry test', () => {
-        assert.equal(baseConfig.entry.index, '/Users/luichooy/Documents/luichooy-code/webpack/webpack-builder/demo/src/pages/index/index.js')
-        assert.equal(baseConfig.entry.search, '/Users/luichooy/Documents/luichooy-code/webpack/webpack-builder/demo/src/pages/search/index.js')
+        assert.equal(baseConfig.entry.index.indexOf('webpack-builder/demo/src/pages/index/index.js') > -1, true)
+        assert.equal(baseConfig.entry.search.indexOf('webpack-builder/demo/src/pages/search/index.js') > -1, true)
     })
 })
