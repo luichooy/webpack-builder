a826d534e7a2d9f8eba550d1eb05a0509c6376c0
luichooy <luichooy@gmail.com>
2020-03-30 00:53:23 +0800

    test: introduce @babel/polyfill

--- demo/src/pages/search/index.js
+++ demo/src/pages/search/index.js
@@ -1,3 +1,4 @@
+import '@babel/polyfill'
 import logo from '@/assets/images/logo.jpg'
 import React from 'react'
 import ReactDom from 'react-dom'
@@ -8,38 +9,38 @@ import '@/assets/styles/search.scss'
 import { cube } from './tree-shaking'
 
 class Search extends React.Component {
-	constructor() {
-		super()
-		this.state = {
-			text: null
-		}
-	}
+  constructor() {
+    super()
+    this.state = {
+      text: null
+    }
+  }
 
-	loadComponent() {
-		import('./dynamic-import.js').then(res => {
-			console.log(res)
-			this.setState({
-				text: res.default
-			})
-		})
-	}
+  loadComponent() {
+    import('./dynamic-import.js').then(res => {
+      console.log(res)
+      this.setState({
+        text: res.default
+      })
+    })
+  }
 
-	render() {
-		const greeting = greet()
-		const { text } = this.state
-		return (
-			<div className='search'>
-				{text && <span>{text}</span>}
-				<span className='greet'>
-					搜索内容
-					{greeting}
-				</span>
-				<span>{common()}</span>
-				<span>{`cube 3 is ${cube(3)}`}</span>
-				<img onClick={this.loadComponent.bind(this)} src={logo} alt='logo' className='logo' />
-			</div>
-		)
-	}
+  render() {
+    const greeting = greet()
+    const { text } = this.state
+    return (
+      <div className="search">
+        {text && <span>{text}</span>}
+        <span className="greet">
+          搜索内容
+          {greeting}
+        </span>
+        <span>{common()}</span>
+        <span>{`cube 3 is ${cube(3)}`}</span>
+        <img onClick={this.loadComponent.bind(this)} src={logo} alt="logo" className="logo" />
+      </div>
+    )
+  }
 }
 
 ReactDom.render(<Search />, document.getElementById('root'))
